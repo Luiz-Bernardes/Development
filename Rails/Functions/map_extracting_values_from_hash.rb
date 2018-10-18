3077f38dc2fef1475192b98f820577b84cdc0a26
Luiz Bernardes <soumaisluiz@hotmail.com>
2018-10-18 20:23:28 -0300

    flatten function

--- Rails/Functions/map_extracting_values_from_hash.rb
+++ Rails/Functions/map_extracting_values_from_hash.rb
@@ -1,5 +1,10 @@
  # extract values from especific field in hash
+channels = Channel.all
 channels = channels.map(&:name).flatten
 
 # with distinct sql attribute
+channels = Channel.all
 channels = channels.map(&:name).flatten.uniq
+
+# function flatten - set elements at same level
+[[1,2,3],[8,9]].flatten # => [1, 2, 3, 8, 9]
