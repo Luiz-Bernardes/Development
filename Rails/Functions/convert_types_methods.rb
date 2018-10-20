0cb4b522fa51ef3b3afb9f0b32442ff8e39a8583
Luiz Bernardes <soumaisluiz@hotmail.com>
2018-10-20 12:29:02 -0300

    convert types

---
+++ Rails/Functions/convert_types_methods.rb
@@ -0,0 +1,16 @@
+# TO INTEGER
+"5".to_i        # 5
+"55.5".to_i     # 55
+"123-abc".to_i  # 123
+
+# TO FLOAT
+"55.5".to_f     # 55.5
+55.to_f         # 55.0
+55.0.to_f       # 55.0
+
+# TO STRING
+1000.to_s       # "1000"
+
+# TO SYMBOL
+"house".to_sym  # :house
+"house".intern  # :house
