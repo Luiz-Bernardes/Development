2f5965e2d5808b92a333c405938277516c7e62ba
Luiz Bernardes <soumaisluiz@hotmail.com>
2018-12-21 17:19:33 -0200

    render json

---
+++ Rails/Api/render_json.rb
@@ -0,0 +1,14 @@
+class UserController < ApplicationController
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    render json: @user
+  end
+end
