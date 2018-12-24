abf38da3952ba8a47d666cbefe16187b4a581f6b
Luiz Bernardes <soumaisluiz@hotmail.com>
2018-12-24 12:33:02 -0200

    status

--- Rails/Api/render_json.rb
+++ Rails/Api/render_json.rb
@@ -1,13 +1,11 @@
 class UserController < ApplicationController
-  # GET /users
-  # GET /users.json
+  
   def index
     @users = User.all
     render json: @users
+    # render json: @users, status: :ok
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     render json: @user
   end
