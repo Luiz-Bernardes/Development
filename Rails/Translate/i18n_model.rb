fc8ccfc82b60a5c4b71cffde5fa688902198791b
Luiz Bernardes <soumaisluiz@hotmail.com>
2019-01-15 12:54:15 -0200

    model translation

---
+++ Rails/Translate/i18n_model.rb
@@ -0,0 +1,14 @@
+# config/locales/models/pt-BR.yml
+pt-BR:
+  activerecord:
+    models:
+      user:
+        one: Usuário
+        other: Usuários
+      product:
+        one: Produto
+        other: Produtos
+
+
+# model translation
+= User.model_name.human count: 2
